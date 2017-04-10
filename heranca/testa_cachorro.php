f5c36b83567304c8c22e646ea7ebfae10a2b1d74
Igor José Brilhante <igor.brilhante@fortaleza.ce.gov.br>
2017-04-10 10:12:55 -0300

    Retirando acentos.

--- heranca/testa_cachorro.php
+++ heranca/testa_cachorro.php
@@ -1,7 +1,7 @@
 <?php
   include_once 'class\Cachorro.php';
 
-  $primeiro_animal = new Cachorro("Pitbull", "Ração", "Late", "Voraz",
+  $primeiro_animal = new Cachorro("Pitbull", "Racao", "Late", "Voraz",
                                   "Encolhido", "Corre");
 
   $primeiro_animal->set_id(1);
@@ -10,4 +10,4 @@
   echo "<b>ID: </b>".$primeiro_animal->get_id()."<br>";
   echo "<b>Tipo de Animal: </b>".$primeiro_animal->get_tipo_animal()."<br>";
   echo "<b>Tipo de Comida: </b>".$primeiro_animal->get_tipo_comida_animal()."<br>";
-  echo "<b>Nível de Fome: </b>".$primeiro_animal->get_nivel_fome_animal()."<br>";
+  echo "<b>Nivel de Fome: </b>".$primeiro_animal->get_nivel_fome_animal()."<br>";
