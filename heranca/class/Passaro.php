2690a3cd0618ffdc5686a6c5c917acef9a248383
Igor José Brilhante <igor.brilhante@fortaleza.ce.gov.br>
2017-04-12 09:33:49 -0300

    Testa objeto Pássaro.

---
+++ heranca/class/Passaro.php
@@ -0,0 +1,18 @@
+<?php
+include_once 'Animal.php';
+
+class Passaro extends Animal {
+
+  public function __construct($tipo_animal, $tipo_comida_animal,
+                              $comportamento_animal_ruido,
+                              $comportamento_animal_comer,
+                              $comportamento_animal_dormindo,
+                              $comportamento_animal_vagueando_ambiente) {
+
+        parent::__construct($tipo_animal, $tipo_comida_animal,
+                            $comportamento_animal_ruido,
+                            $comportamento_animal_comer,
+                            $comportamento_animal_dormindo,
+                            $comportamento_animal_vagueando_ambiente);
+  }
+}
