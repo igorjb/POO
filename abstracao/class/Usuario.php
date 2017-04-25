d0e81a91a4c0602c522220a750cdf4fb7767e5c9
Igor José Brilhante <igor.brilhante@fortaleza.ce.gov.br>
2017-04-25 10:13:22 -0300

    Testa classe abstrata.

---
+++ abstracao/class/Usuario.php
@@ -0,0 +1,51 @@
+<?php
+  abstract class Usuario {
+    private $id;
+    private $usuario;
+    private $senha;
+    private $email;
+    private $tipo_conta;
+
+    //métodos são funções!!!
+    public function __construct($novo_usuario, $nova_senha){
+      $this->usuario = $novo_usuario;
+      $this->senha = $nova_senha;
+    }
+
+    public function logar() {
+      echo "Login de Usuário qualquer";
+    }
+
+    public function set_id($novo_id){
+      $this->id = $novo_id;
+    }
+
+    public function get_id(){
+      return $this->id;
+    }
+
+    public function get_usuario(){
+      return $this->usuario;
+    }
+
+    public function get_senha(){
+      return $this->senha;
+    }
+
+
+    public function set_email($novo_email){
+      $this->email = $novo_email;
+    }
+
+    public function get_email(){
+      return $this->email;
+    }
+
+    public function set_tipo_conta($novo_tipo_conta){
+      $this->tipo_conta = $novo_tipo_conta;
+    }
+
+    public function get_tipo_conta(){
+      return $this->tipo_conta;
+    }
+  }
---
+++ abstracao/testa_aluno.php
@@ -0,0 +1,9 @@
+<?php
+
+  include_once 'class/Aluno.php';
+
+  $primeiro_aluno = new Aluno("Igor", "123", date("Ymd"));
+
+  echo "Meu Usuario &eacute;: ".$primeiro_aluno->get_usuario().'<br>';
+  echo "Minha Senha &eacute;: ".$primeiro_aluno->get_senha().'<br>';
+  echo "Minha Matricula &eacute;: ".$primeiro_aluno->get_matricula().'<br>';
